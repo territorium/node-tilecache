79ee3f53fbbbbc85f465a0c6fe9f055f33abb68e
claudio <claudio@claudio-ubuntu.(none)>
2012-08-17 21:05:45 +0200

    aggiunto la funzione crea

--- index.js
+++ index.js
@@ -25,9 +25,29 @@ var server = http.createServer(function (request, response) {
         else if (urlArray.length == 5){
             var mappe = require('./mappe.js');
             not_found = mappe.mappa(response, config, urlArray);
-                                // }
             }
-                   if (not_found) {
+        else if (urlArray.length == 8){
+            console.log(urlArray);
+            var mappe = require('./mappe.js');
+            if (urlArray[2] == 'create'){
+                var level = urlArray[3];
+                var xini = urlArray[4];
+                var xfin = urlArray[5];
+                var yini = urlArray[6];
+                var yfin = urlArray[7];
+                var i = 0, j = 0;
+               // var newArray = [urlArray[0], urlArray[1], urlArray[3], '0', '1'];
+              //  not_found = mappe.mappa(response, config, newArray);
+                for (i = xini; i <= xfin; i++){
+                    for (j = yini; j <= yfin; j++){
+                        var newArray = [urlArray[0], urlArray[1], urlArray[3], i + '', j + ''];
+                        console.log(newArray);
+                    mappe.crea(response, config, newArray);
+                    }
+                }
+            }
+        }
+        if (not_found) {
             response.statusCode = 404;
             response.setHeader('Content-Type', 'text/plain');
             response.end('Not Found');}		
